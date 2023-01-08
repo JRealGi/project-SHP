fd96ac65a7ef2e1e3fb39d913e8b5943c7a65ae3
JReal <373301748@qq.com>
2023-01-09 01:54:13 +0800

    2023/1/9 1:54

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
+import TypeNav from "@/pages/Home/TypeNav";
+
+Vue.component(TypeNav.name, TypeNav);
 //注册路由
-import router from '@/router';
+import router from "@/router";
+
+import { reqCategoryList } from "@/api";
+reqCategoryList();
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   //注册路由:底下的写法KV一致省略V【router小写的】
-  router
-}).$mount('#app')
+  router,
+}).$mount("#app");
