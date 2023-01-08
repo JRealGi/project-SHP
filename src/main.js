6dd05ea5eef7fc6c61902a58458f6ffe7f08fa02
JReal <373301748@qq.com>
2023-01-08 22:24:11 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//注册路由
+import router from '@/router';
 
 new Vue({
   render: h => h(App),
+  //注册路由:底下的写法KV一致省略V【router小写的】
+  router
 }).$mount('#app')
