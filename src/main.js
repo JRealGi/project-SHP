f37693840f67c8b032d63fc57794e919063893a7
JReal <373301748@qq.com>
2023-01-10 16:27:48 +0800

    2023/1/10 16:27

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import TypeNav from "@/pages/Home/TypeNav";
+import TypeNav from "@/components/TypeNav";
 
 Vue.component(TypeNav.name, TypeNav);
 //注册路由
 import router from "@/router";
 
 import store from "@/store";
-
-import { reqCategoryList } from "@/api";
-reqCategoryList();
 
 new Vue({
   render: (h) => h(App),
